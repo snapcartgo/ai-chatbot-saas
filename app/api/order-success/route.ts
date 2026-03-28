72deaa32d4fb870a92da5a2838f8a3549fba0850
snapcartgo <amitmutreja2186@gmail.com>
2026-03-28 14:49:23 +0530

    Updated chatbot widget and dashboard fixes

--- app/api/order-success/route.ts
+++ app/api/order-success/route.ts
@@ -5,7 +5,6 @@ export async function POST(req: Request) {
 
   const order_id = formData.get("txnid");
 
-  // 🔁 Redirect to UI page
   return NextResponse.redirect(
     `https://ai-chatbot-saas-five.vercel.app/order-success?order_id=${order_id}`
   );
