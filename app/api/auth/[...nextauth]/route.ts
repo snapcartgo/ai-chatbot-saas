32ef6c6b5e73298ba73f363c2176888dc3d7a39b
snapcartgo <amitmutreja2186@gmail.com>
2026-05-22 15:17:50 +0530

    updated Webhook2

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,10 +4,11 @@ import FacebookProvider from "next-auth/providers/facebook";
 const handler = NextAuth({
   providers: [
     FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID!,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+      clientId: process.env.NEXT_PUBLIC_FACEBOOK_APP_ID!,
+      clientSecret: process.env.FACEBOOK_APP_SECRET!,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
